b34979d9116d8822f247d4248919a7f5fdd053a5
mikescandy <michele.scandura@outlook.com>
2014-10-21 20:48:35 +0100

    work in progress

--- ArcgisXamarinBinding/Additions/ArcGISMediaAdapter.cs
+++ ArcgisXamarinBinding/Additions/ArcGISMediaAdapter.cs
@@ -8,9 +8,9 @@ namespace Com.Esri.Android.Map.Popup
 {
     public partial class ArcGISMediaAdapter
     {
-        Object IAdapter.GetItem(int position)
-        {
-            return ((IAdapter)this).GetItem(position);
-        }
+        //Object IAdapter.GetItem(int position)
+        //{
+        //    return ((IAdapter)this).GetItem(position);
+        //}
     }
 }
